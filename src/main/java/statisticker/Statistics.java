9d5b06f25d743e9d14002e7c0b1fef093d9e5e91
Mugundhan <74087749+Mugundhan-D@users.noreply.github.com>
2021-01-27 13:40:37 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -1,10 +1,66 @@
 package statisticker;
 
+import java.util.Collections;
 import java.util.List;
 
-public class Statistics 
-{
-    public static Stats getStatistics(List<___> numbers) {
-        //implement the computation of statistics here
-    }
+public class Statistics {
+	public static class Stats {
+		public float average = Float.NaN;
+		public float min = Float.NaN;
+		public float max = Float.NaN;
+
+		public Stats(float average, float max, float min) {
+			this.average = average;
+			this.max = max;
+			this.min = min;
+		}
+
+		public Stats() {
+
+		}
+
+		public float getAverage() {
+			return average;
+		}
+
+		public void setAverage(float average) {
+			this.average = average;
+		}
+
+		public float getMin() {
+			return min;
+		}
+
+		public void setMin(float min) {
+			this.min = min;
+		}
+
+		public float getMax() {
+			return max;
+		}
+
+		public void setMax(float max) {
+			this.max = max;
+		}
+	}
+
+	public static Stats getStatistics(List<Float> numbers) {
+		if (numbers == null || numbers.isEmpty()) {
+			return new Stats();
+		} else {
+			float averageofNumbers = getAverageofNumbers(numbers);
+			float max = Collections.max(numbers);
+			float min = Collections.min(numbers);
+			Stats stats = new Stats(averageofNumbers, max, min);
+			return stats;
+		}
+	}
+
+	private static float getAverageofNumbers(List<Float> numbers) {
+		float sum = 0;
+		for (float mark : numbers) {
+			sum += mark;
+		}
+		return sum / numbers.size();
+	}
 }
